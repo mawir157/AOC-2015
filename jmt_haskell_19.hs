16d59c76b984793707cbc3c8551b9ffe90f1c21f
james thompson <jamesmthompson2@gmail.com>
2020-06-21 11:28:31 +0100

    Day 19 part 2

--- jmt_haskell_19.hs
+++ jmt_haskell_19.hs
@@ -1,4 +1,5 @@
-import Data.List
+import Data.List 
+import Data.Ord
 import Data.List.Split
 
 import Debug.Trace
@@ -21,11 +22,6 @@ applyRule :: String -> Rule -> [String]
 applyRule s (f,t) = map (\n -> applyRule' s (f,t) n) is
   where is = search f s
 
--- rep :: String -> ([String],Int) -> [Rules] -> ([String],Int)
--- rep target (seen, count) rules
---   | elem target seen = (seen, count)
---   | otherwise        = 
-
 revRule :: String -> Rule -> [String]
 revRule s (f,t) = map (\n -> applyRule' s (t,f) n) is
   where is = search t s
@@ -33,22 +29,29 @@ revRule s (f,t) = map (\n -> applyRule' s (t,f) n) is
 revAllRules :: [Rule] -> String -> [String]
 revAllRules rs s = concat $ map (revRule s) rs
 
+shortest :: Int -> [[a]] -> [[a]]
+shortest n xs = take n $ sortBy (comparing length) xs
+
+longest :: Int -> [[a]] -> [[a]]
+longest n xs = take n $ reverse $ sortBy (comparing length) xs
+
 reduce :: [String] -> [Rule] -> [String]
 reduce seen rules = seen'
-  where seen' = nub $ concat $ map (revAllRules rules) seen
+  where seen' = nub $ concat $ longest 1 $ map (revAllRules rules) seen
 
 reduceToEnd :: ([String],Int) -> [Rule] -> ([String],Int)
 reduceToEnd (ss,n) rs
   | elem "e" ss = (ss, n)
   | otherwise   = (reduceToEnd (reduce ss rs, n+1)) rs
+  -- where !debug = traceShowId $ minimumBy (comparing length) ss
 
 main = do
   f <- readFile "input_19.txt"
   let rules = map parseInput $ init $ init $ lines f
   let molecule = last $ lines f
 
-  let rules = [("e","H"),("e","O"),("H","HO"),("H","OH"),("O", "HH")]
-  let molecule = "HOHOHO"
+  -- let rules = [("e","H"),("e","O"),("H","HO"),("H","OH"),("O", "HH")]
+  -- let molecule = "HOHOHO"
 
   let w = nub $ concat $ map (applyRule molecule) rules
 
@@ -56,25 +59,4 @@ main = do
   putStrLn $ show $ length w
 
   putStr "Part 2: "
-  putStrLn $ show $ length molecule
-  -- putStrLn $ show $ snd $ reduceToEnd ([molecule], 0) rules
-
-  let t1 = reduce [molecule] rules
-  putStrLn $ show $ length t1 
-  let t2 = reduce t1 rules
-  putStrLn $ show $ length t2 
-
-  let t1 = reduce t2 rules
-  putStrLn $ show $ length t1 
-  let t2 = reduce t1 rules
-  putStrLn $ show $ length t2 
-
-  let t1 = reduce t2 rules
-  putStrLn $ show $ length t1 
-  let t2 = reduce t1 rules
-  putStrLn $ show $ length t2 
-
-  let t1 = reduce t2 rules
-  putStrLn $ show $ length t1 
-  let t2 = reduce t1 rules
-  putStrLn $ show $ length t2 
+  putStrLn $ show $ snd $ reduceToEnd ([molecule], 0) rules
