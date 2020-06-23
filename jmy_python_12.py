4aa2c970e15ef0be00b01a75e398ab0131c30963
james thompson <jamesmthompson2@gmail.com>
2020-06-23 14:35:37 +0100

    Golf

--- jmy_python_12.py
+++ jmy_python_12.py
@@ -2,38 +2,26 @@
 # it another language
 import json
 
-def arrSum(arr, part2):
-  total = 0
-  for x in arr:
-    if type(x) is list:
-      total += arrSum(x, part2)  
-    if type(x) is int:
-      total += x
-    if type(x) is dict:
-      total += dictsum(x, part2)  
-
-  return total 
-
-def dictsum(dt, part2=False):
+def parseSum(dt, mode=0, part2=False):
   total = 0
   for x in dt:
-    k = dt[x]
-    if (part2 and k == "red"):
+    if mode == 0:
+      x = dt[x]
+
+    if (mode == 0 and part2 and x == "red"):
         return 0
 
-    if type(k) is list:
-      total += arrSum(k, part2)  
-    if type(k) is int:
-      total += k 
-    if type(k) is dict:
-      total += dictsum(k, part2)
+    if type(x) is list:
+      total += parseSum(x, 1, part2)  
+    elif type(x) is int:
+      total += x
+    elif type(x) is dict:
+      total += parseSum(x, 0, part2)
 
   return total
 
 with open('input_12.txt') as json_file:
     data = json.load(json_file)
 
-    print("Part 1: ")
-    print(dictsum(data))
-    print("Part 2: ")
-    print(dictsum(data,True))
+    print("Part 1: " + str(parseSum(data)))
+    print("Part 2: " + str(parseSum(data,0,True)))
